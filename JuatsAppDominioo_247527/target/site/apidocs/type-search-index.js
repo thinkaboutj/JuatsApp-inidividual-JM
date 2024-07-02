f5c1177082056c7c9f417cd6861ddba9ec30411d
Jesus <159750556+thinkaboutj@users.noreply.github.com>
2024-07-02 09:24:57 -0700

    x

--- JuatsAppDominioo_247527/target/site/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"dominio","l":"Chat"},{"p":"dominio","l":"Domicilio"},{"p":"dominio","l":"GeneroUsuario"},{"p":"dominio","l":"Imagen"},{"p":"dominio","l":"Mensaje"},{"p":"dominio","l":"Usuario"}];updateSearchResults();
